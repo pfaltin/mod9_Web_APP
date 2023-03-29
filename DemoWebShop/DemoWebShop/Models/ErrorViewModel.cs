f722692f8816c1aacb8390d9f22b511828fe2732
Petar Faltin <petar.faltin@gmail.com>
2023-03-29 19:14:01 +0200

    inicijalno kreiranje projekta
    MVC VS

---
+++ DemoWebShop/DemoWebShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoWebShop.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
