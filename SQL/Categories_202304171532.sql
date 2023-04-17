dffce7eff3d667b072b38b392ade697e85da538e
pfaltin <pfaltin@yahoo.com>
2023-04-17 15:35:14 +0200

    SQL

---
+++ SQL/Categories_202304171532.sql
@@ -0,0 +1,18 @@
+INSERT INTO m9_webshop.dbo.Categories (Title,Description,[Image]) VALUES
+	 (N'Pome fruit',N'Fruits that have a core with seeds surrounded by flesh, such as apples and pears.',N'categoryImage.png'),
+	 (N'Berry fruit',N'Small, juicy fruits with a thin skin and lots of seeds, such as blueberries, raspberries, and grapes.',N'categoryImage.png'),
+	 (N'Citrus fruit',N'Juicy fruits with a pulpy flesh and a thick rind, such as oranges, lemons, and limes.',N'categoryImage.png'),
+	 (N'Tropical fruit',N'Fruits that grow in warm climates, such as pineapples, bananas, and mangoes.',N'categoryImage.png'),
+	 (N'Pepo fruit',N'Fruits with a tough rind and fleshy interior, such as watermelons, cantaloupes, and pumpkins.',N'categoryImage.png'),
+	 (N'Stone fruit',N'Fruits with a hard, stone-like pit in the center, such as cherries, peaches, and plums.',N'categoryImage.png'),
+	 (N'Root vegetable',N'Vegetables that grow underground and store energy in their roots, such as carrots, beets, and potatoes.',N'categoryImage.png'),
+	 (N'Brassica vegetable',N'Vegetables that belong to the cabbage family, such as broccoli, cauliflower, and Brussels sprouts.',N'categoryImage.png'),
+	 (N'Fruit vegetable',N'Vegetables that are technically fruits, such as tomatoes, cucumbers, and peppers.',N'categoryImage.png'),
+	 (N'Tubers',N'Vegetables that grow underground and store energy in their tuberous roots, such as potatoes and sweet potatoes.',N'categoryImage.png');
+INSERT INTO m9_webshop.dbo.Categories (Title,Description,[Image]) VALUES
+	 (N'Leafy green vegetable',N'Vegetables with leaves that are eaten raw or cooked, such as kale, spinach, and lettuce.',N'categoryImage.png'),
+	 (N'Bulb vegetable',N'Vegetables that have a bulbous base, such as onions and garlic.',N'categoryImage.png'),
+	 (N'Legumes',N'Vegetables that are high in protein and grow in pods, such as peas, beans, and lentils.',N'categoryImage.png'),
+	 (N'Grain',N'Seeds of cereal plants that are harvested and used for food, such as corn, wheat, and rice.',N'categoryImage.png'),
+	 (N'Shoot vegetable',N'Vegetables that are harvested as tender, young shoots, such as asparagus and bamboo shoots.',N'categoryImage.png'),
+	 (N'Flower bud vegetable',N'Vegetables that are harvested as flower buds before they bloom, such as artichokes and broccoli.',N'categoryImage.png');
---
+++ SQL/Products_202304171532.sql
@@ -0,0 +1,37 @@
+INSERT INTO m9_webshop.dbo.Products (Title,Description,InStock,Price,[Image],Sku) VALUES
+	 (N'Apple',N'Fresh, juicy apple from the local orchard',100.00,1.25,N'prodImage.png',N'K0001'),
+	 (N'Banana',N'Ripe, yellow banana picked at peak ripeness',75.00,0.75,N'prodImage.png',N'K0002'),
+	 (N'Carrot',N'Organic, locally grown carrot with vibrant color',50.00,0.50,N'prodImage.png',N'K0003'),
+	 (N'Blueberries',N'Sweet and tangy blueberries freshly picked',30.00,2.50,N'prodImage.png',N'K0004'),
+	 (N'Kale',N'Leafy green kale with a crisp texture',20.00,1.00,N'prodImage.png',N'K0005'),
+	 (N'Orange',N'Juicy, citrusy orange perfect for snacking',60.00,1.50,N'prodImage.png',N'K0006'),
+	 (N'Spinach',N'Nutritious spinach packed with vitamins and minerals',15.00,0.75,N'prodImage.png',N'K0007'),
+	 (N'Watermelon',N'Sweet and refreshing watermelon, perfect for summer',10.00,5.00,N'prodImage.png',N'K0008'),
+	 (N'Strawberries',N'Plump, juicy strawberries with a bright red color',40.00,3.00,N'prodImage.png',N'K0009'),
+	 (N'Broccoli',N'Fresh, crunchy broccoli loaded with nutrients',25.00,1.25,N'prodImage.png',N'K0010');
+INSERT INTO m9_webshop.dbo.Products (Title,Description,InStock,Price,[Image],Sku) VALUES
+	 (N'Cucumber',N'Crisp, cool cucumber ideal for salads and sandwiches',35.00,0.50,N'prodImage.png',N'K0011'),
+	 (N'Avocado',N'Creamy, rich avocado perfect for guacamole',5.00,2.00,N'prodImage.png',N'K0012'),
+	 (N'Pineapple',N'Tropical, juicy pineapple bursting with flavor',10.00,3.50,N'prodImage.png',N'K0013'),
+	 (N'Tomato',N'Vibrant, ripe tomato great for sandwiches and salads',45.00,0.75,N'prodImage.png',N'K0014'),
+	 (N'Mango',N'Sweet, juicy mango perfect for smoothies and desserts',15.00,2.50,N'prodImage.png',N'K0015'),
+	 (N'Potato',N'Versatile potato great for frying, baking, or mashing',30.00,0.50,N'prodImage.png',N'K0016'),
+	 (N'Grapes',N'Plump, juicy grapes bursting with flavor',20.00,2.00,N'prodImage.png',N'K0017'),
+	 (N'Zucchini',N'Fresh, tender zucchini perfect for grilling or sautéing',10.00,0.75,N'prodImage.png',N'K0018'),
+	 (N'Cantaloupe',N'Sweet and succulent cantaloupe, perfect for breakfast',8.00,4.00,N'prodImage.png',N'K0019'),
+	 (N'Peppers',N'Colorful, crisp peppers great for stuffing or stir-fry',15.00,1.50,N'prodImage.png',N'K0020');
+INSERT INTO m9_webshop.dbo.Products (Title,Description,InStock,Price,[Image],Sku) VALUES
+	 (N'Cauliflower',N'Fresh, tender cauliflower loaded with nutrients',10.00,1.00,N'prodImage.png',N'K0021'),
+	 (N'Lemon',N'Tangy, bright lemon ideal for adding flavor to dishes',25.00,0.50,N'prodImage.png',N'K0022'),
+	 (N'Cherry',N'Sweet and tart cherries perfect for snacking or baking',12.00,3.00,N'prodImage.png',N'K0023'),
+	 (N'Onion',N'Flavorful onion great for sautéing or roasting',40.00,0.75,N'prodImage.png',N'K0024'),
+	 (N'Eggplant',N'Versatile eggplant great for grilling or roasting',5.00,1.50,N'prodImage.png',N'K0025'),
+	 (N'Peas',N'Fresh, tender peas packed with vitamins',20.00,1.25,N'prodImage.png',N'K0026'),
+	 (N'Corn',N'Sweet, crisp corn perfect for grilling or boiling',15.00,0.75,N'prodImage.png',N'K0027'),
+	 (N'Asparagus',N'Tender, flavorful asparagus great for roasting or grilling',10.00,2.00,N'prodImage.png',N'K0028'),
+	 (N'Artichoke',N'Hearty, flavorful artichoke ideal for steaming or roasting',5.00,3.50,N'prodImage.png',N'K0029'),
+	 (N'Green beans',N'Crisp, tender green beans loaded with nutrients',25.00,1.00,N'prodImage.png',N'K0030');
+INSERT INTO m9_webshop.dbo.Products (Title,Description,InStock,Price,[Image],Sku) VALUES
+	 (N'Squash',N'Fresh, tender squash great for grilling or sautéing',10.00,1.25,N'prodImage.png',N'K0031'),
+	 (N'Pumpkin',N'Hearty, flavorful pumpkin perfect for soups or pies',7.00,4.00,N'prodImage.png',N'K0032'),
+	 (N'Radish',N'Crunchy, spicy radish great for salads or pickling',20.00,0.50,N'prodImage.png',N'K0033');
